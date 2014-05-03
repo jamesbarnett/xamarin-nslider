8e2f572955b98eeb2ec57d43d5d820967b6f7fcb
Jim Barnett <james.barnett.jr@gmail.com>
2014-05-03 17:42:10 -0500

    Fix some trivial code formatting

--- Test/Test.cs
+++ Test/Test.cs
@@ -4,17 +4,18 @@ using Game;
 
 namespace Test
 {
-	[TestFixture ()]
+	[TestFixture()]
 	public class Test
 	{
-		[Test ()]
-		public void TestCase ()
+		[Test()]
+		public void TestCase()
 		{
 			var game = new Puzzle(3);
 			var solver = new Solver(game);
 			var solution = solver.Solution();
 			
-			foreach (var step in solution) {
+			foreach (var step in solution)
+            {
 				Console.WriteLine(string.Format("Step is #{0}", step));
 			}
 		}
